5105684b18f7b2519a7840397797ab62afbe34e0
Tobias <tobiaswhd@gmail.com>
2018-11-17 22:35:52 +0000

    genesis commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Container from './components/layout/Container';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const styles = {
+  root: {
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  main: {
+    flex: 1,
+  },
+};
+
+const App = () => (
+  <div style={styles.root}>
+    <Header/>
+    <main style={styles.main}>
+      <Container>
+
+        // @TODO add routing
+      </Container>
+    </main>
+    <Footer/>
+  </div>
+);
 
 export default App;
